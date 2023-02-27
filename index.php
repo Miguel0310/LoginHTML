91a60ffce949b005982c81c9af2c826be983c833
Miguel0310 <ssl.miguel95.mn@gmail.com>
2023-02-26 21:34:52 -0300

    Primeira versao da tela de login

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Login Project</title>
+</head>
+
+<body>
+
+    <div class="main-login">
+        <div class="left-login">
+            <h1>Faça Login <br>E entre para nosso time</h1>
+            <img src="animated-astro.svg" class="left-login-image" alt="">
+        </div>
+        <div class="right-login">
+            <div class="card-login">
+                <h1>Login</h1>
+                <div class="textfield">
+                    <label for="usuario">Usuário</label>
+                    <input type="text" name="usuario" placeholder="Usuário">
+                    <label for="Senha">Senha</label>
+                    <input type="password" name="Senha" placeholder="Senha">
+                </div>
+                <button class="btn-login">Login</button>
+
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
